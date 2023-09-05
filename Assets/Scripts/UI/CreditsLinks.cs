c19575bdc504da57a06b7fc2848b9bc53d13feff
Marek Nowakowski <marsznow@gmail.com>
2023-09-06 00:21:22 +0200

    Credits panel added

---
+++ Assets/Scripts/UI/CreditsLinks.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CreditsLinks : MonoBehaviour
+{
+    public void Developer()
+    {
+        Application.OpenURL("https://mareksnowakowski.github.io");
+    }
+    
+    public void Environment()
+    {
+        Application.OpenURL("https://szadiart.itch.io/pixel-fantasy-caves");
+    }
+    
+    public void Player()
+    {
+        Application.OpenURL("https://rvros.itch.io/animated-pixel-hero");
+    }
+    
+    public void Music()
+    {
+        Application.OpenURL("https://tallbeard.itch.io/music-loop-bundle");
+    }
+    
+    public void Icons()
+    {
+        Application.OpenURL("https://assetstore.unity.com/packages/2d/gui/icons/2d-simple-ui-pack-218050");
+    }
+}
